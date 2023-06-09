2000c9d50ab90b94526d2d07044dee93f445baaf
cutberto <cutbertoan@gmail.com>
2023-06-08 18:14:57 -0600

    Prototipo de app web para usar modelos

--- app/back/app.py
+++ app/back/app.py
@@ -16,28 +16,13 @@ def tokenize(code1):
 import tensorflow as tf
 import tensorflow_text as text
 
-def compare_code_rouge_l(code_A, code_B, plagium_value):
-
-    tokenizer = text.WhitespaceTokenizer()
-
-    # Tokenize code A and code B
-    code_A = tokenizer.tokenize([code_A])
-    code_B = tokenizer.tokenize([code_B])
-
-    # Compute Rouge-L
-    result = text.metrics.rouge_l(code_A, code_B)
-    return int(result.f_measure)
-    if result.f_measure >= plagium_value:
-        return 1
-    else:
-        return 0
-
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 def compare_tokens_tfidf(tokens1, tokens2, plagium_value):
-
+    print(tokens1,"\n",tokens2)
     # Crear un objeto TfidfVectorizer
+
     vectorizer = TfidfVectorizer()
 
     # Aplicar la vectorización TF-IDF a las cadenas de texto
@@ -46,24 +31,106 @@ def compare_tokens_tfidf(tokens1, tokens2, plagium_value):
     # Calcular la similitud del coseno entre los vectores TF-IDF
     similarity = cosine_similarity(tfidf_matrix[0], tfidf_matrix[1])
     return similarity[0][0]
-    if similarity[0][0] >= plagium_value:
+
+import re 
+import numpy as np
+from collections import defaultdict
+def calculate_transition_matrix(text):
+    # Eliminar caracteres no alfanuméricos y convertir a minúsculas
+    text = re.sub(r'[^a-zA-Z0-9\s]', '', text.lower())
+
+    words = text.split()  # Dividir el texto en palabras
+    unique_words = sorted(list(set(words)))  # Obtener las palabras únicas en el texto
+    word_indices = {word: i for i, word in enumerate(unique_words)}  # Asignar un índice numérico a cada palabra
+
+    matrix = np.zeros((len(unique_words), len(unique_words)))  # Crear una matriz de ceros para la matriz de transición
+
+    # Calcular las frecuencias de transición entre palabras consecutivas en el texto
+    transitions = defaultdict(int)
+    for i in range(len(words)-1):
+        current_word = words[i]
+        next_word = words[i+1]
+        transitions[(current_word, next_word)] += 1
+
+    # Llenar la matriz de transición con las frecuencias de transición
+    for (current_word, next_word), count in transitions.items():
+        current_index = word_indices[current_word]
+        next_index = word_indices[next_word]
+        matrix[current_index, next_index] = count
+
+    # Normalizar las filas de la matriz para obtener probabilidades de transición
+    row_sums = np.sum(matrix, axis=1)
+    row_sums[row_sums == 0] = 1  # Reemplazar los valores de suma cero por 1 para evitar divisiones entre cero
+    matrix = matrix / row_sums[:, np.newaxis]
+
+    return matrix, unique_words
+
+
+
+def cosine_ang(a, b):
+    # Obtener las dimensiones de las matrices
+    rows1, cols1 = a.shape
+    rows2, cols2 = b.shape
+
+    # Ajustar las dimensiones de las matrices si son diferentes
+    if rows1 < rows2:
+        a = np.vstack((a, np.zeros((rows2 - rows1, cols1))))
+    elif rows1 > rows2:
+        b = np.vstack((b, np.zeros((rows1 - rows2, cols2))))
+
+    if cols1 < cols2:
+        a = np.hstack((a, np.zeros((a.shape[0], cols2 - cols1))))
+    elif cols1 > cols2:
+        b = np.hstack((b, np.zeros((b.shape[0], cols1 - cols2))))
+
+    aT = a.transpose()
+    bT = b.transpose()
+    C = np.dot(bT, a)
+    prod_int = C.trace()
+
+    normA = np.sqrt(np.dot(aT, a).trace())
+    normB = np.sqrt(np.dot(bT, b).trace())
+
+    cos_ang = prod_int / (normA * normB)
+
+    return cos_ang
+
+
+def compare_tokens_transition_matrix(a,b,plagium_value):
+    a_matrix, words = calculate_transition_matrix(a)
+    b_matrix, words = calculate_transition_matrix(b)
+    cos_ang = cosine_ang(a_matrix,b_matrix)
+    return cos_ang
+    if cos_ang > plagium_value:
         return 1
     else:
         return 0
 
 
-
-@app.route('/compare', methods=['POST'])
-def compare_texts():
+@app.route('/tfidf', methods=['POST'])
+def compare_texts_tfidf():
     
     text1 = request.json['text1']
     text2 = request.json['text2']
 
-    # Perform the text comparison logic here and get the result as a value between 0 and 1
-    #result = compare_code_rouge_l(text1,text2,0.999)  # Replace with your actual comparison logic
-    result = compare_tokens_tfidf(text1,text2,0.999)  # Replace with your actual comparison logic
+    text1 = tokenize(text1)
+    text2 = tokenize(text2)
+    result = compare_tokens_tfidf(text1,text2,0.999)  
 
     return jsonify({'value': result})
 
+
+
+@app.route('/transition', methods=['POST'])
+def compare_texts_transition():
+    lexer = Lexer()
+    text1 = request.json['text1']
+    text2 = request.json['text2']
+    text1 = tokenize(text1)
+    text2 = tokenize(text2)
+
+    result = compare_tokens_transition_matrix(text1,text2,0.999) 
+    
+    return jsonify({'value': result})
 if __name__ == '__main__':
     app.run()
--- app/front/script.js
+++ app/front/script.js
@@ -1,17 +1,43 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll("select");
+  var instances = M.FormSelect.init(elems);
+});
+
 document.getElementById("compareButton").addEventListener("click", function () {
+  var selectedRoute = document.getElementById("backendSelect").value;
   var text1 = document.getElementById("text1").value;
   var text2 = document.getElementById("text2").value;
 
+  // Hide Compare button
+  hideCompareButton();
+
+  // Show loading spinner
+  showLoadingSpinner();
+
   var xhr = new XMLHttpRequest();
-  xhr.open("POST", "http://127.0.0.1:5000/compare", true);
+  xhr.open("POST", selectedRoute, true);
   xhr.setRequestHeader("Content-Type", "application/json");
+
   xhr.onreadystatechange = function () {
-    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-      var response = JSON.parse(xhr.responseText);
-      var value = response.value;
-      updateProgressBar(value);
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      // Hide loading spinner
+      hideLoadingSpinner();
+
+      if (xhr.status === 200) {
+        var response = JSON.parse(xhr.responseText);
+        var value = response.value;
+        updateProgressBar(value);
+        showResultSection(value);
+      } else {
+        // Handle error
+        console.error("Request failed with status:", xhr.status);
+      }
+
+      // Show Compare button
+      showCompareButton();
     }
   };
+
   var data = JSON.stringify({ text1: text1, text2: text2 });
   xhr.send(data);
 });
@@ -19,4 +45,46 @@ document.getElementById("compareButton").addEventListener("click", function () {
 function updateProgressBar(value) {
   var progressBar = document.getElementById("progressBar");
   progressBar.style.width = value * 100 + "%";
+}
+
+function showResultSection(value) {
+  var resultSection = document.getElementById("resultSection");
+  var resultLabel = document.getElementById("resultLabel");
+  var trimmedValue = (value * 100).toFixed(2);
+  resultLabel.innerText = "The similarity is " + trimmedValue + "%";
+  resultSection.classList.remove("hide");
+}
+
+function showCompareButton() {
+  var compareButton = document.getElementById("compareButton");
+  compareButton.classList.remove("hide");
+}
+
+function hideCompareButton() {
+  var compareButton = document.getElementById("compareButton");
+  compareButton.classList.add("hide");
+}
+
+function showLoadingSpinner() {
+  var compareButton = document.getElementById("compareButton");
+  compareButton.innerHTML = `
+    <div class="preloader-wrapper small active">
+      <div class="spinner-layer spinner-blue-only">
+        <div class="circle-clipper left">
+          <div class="circle"></div>
+        </div>
+        <div class="gap-patch">
+          <div class="circle"></div>
+        </div>
+        <div class="circle-clipper right">
+          <div class="circle"></div>
+        </div>
+      </div>
+    </div>
+  `;
+}
+
+function hideLoadingSpinner() {
+  var compareButton = document.getElementById("compareButton");
+  compareButton.innerHTML = "Compare";
 }
